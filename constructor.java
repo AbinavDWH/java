178f1cf28dae0f7f77e09067443f44033c95ee6a
AbinavDWH <ans.offical.net@gmail.com>
2025-07-23 11:16:21 +0530

    new file:   .vscode/launch.json 	new file:   JavaIfElse.java 	new file:   Java_Stdin_and_Stdout_II.java 	new file:   bank.java 	new file:   constructor.java 	new file:   pattten.java

---
+++ constructor.java
@@ -0,0 +1,22 @@
+class achu{
+    achu(){
+        System.out.println("hi,achu");
+    }
+
+    void adi(){
+        System.out.println("hi,adhi");
+    }
+}
+
+
+
+
+
+
+public class constructor {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        achu a = new achu();
+        a.adi();
+    }
+}

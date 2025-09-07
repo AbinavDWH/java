2d484b83a37753c959537ce4118f2abb0f6299e3
AbinavDWH <ans.offical.net@gmail.com>
2025-09-07 10:32:54 +0530

    modified:   hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 class hello{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String s1 = "Hello World";
+        String s2 = "Hello World";
+        String s3 = "Hello World";
+        String s4="bkjjfdsbn";
+        // s4="Hello World";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3==s4);
     }
 }
